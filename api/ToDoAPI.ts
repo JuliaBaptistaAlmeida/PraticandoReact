741b517da2c698b4a14fdad90cf034d68743b14e
Julia Baptista <contatojuliabaptista@hotmail.com>
2026-04-16 13:43:40 -0300

    Create ToDoAPI.ts

---
+++ api/ToDoAPI.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const axiosinstance = axios.create({});
+
+interface IToDo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+interface IToDoWithoutID {
+    title: string;
+    completed: boolean;
+}
+
+export const ToDoAPI = {
+    // Pegar todas
+    async getAll() {
+        const response = await axiosinstance.get('/api/to_dos');
+        return response.data.toDos as IToDo[]; // ✅ plural
+    },
+
+    // Criar
+    async create(data: IToDoWithoutID) {
+        const response = await axiosinstance.post('/api/to_dos', data);
+        console.log("CREATE RESPONSE:", response.data);
+        return response.data.toDos as IToDo; // ✅ singular
+    },
+
+    // Atualizar
+    async update(id: string, data: Partial<IToDoWithoutID>) {
+        const response = await axiosinstance.put(`/api/to_dos/${id}`, data);
+        return response.data.toDos as IToDo; // ✅ singular
+    },
+
+    // Remover
+    async remove(id: string) {
+        await axiosinstance.delete(`/api/to_dos/${id}`);
+    }
+};
