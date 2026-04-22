18f606925e7c46211637eeff6919f6fff33e0133
Julia Baptista <contatojuliabaptista@hotmail.com>
2026-04-22 10:28:28 -0300

    Update server.ts

--- mocks/server.ts
+++ mocks/server.ts
@@ -1,35 +1,43 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Model, Response } from 'miragejs';
+
+function todoPlain(m: { attrs: Record<string, unknown> }) {
+  return { ...m.attrs };
+}
 
 createServer({
   models: {
-    to_dos: Model
+    to_dos: Model,
   },
 
   routes() {
     this.namespace = 'api';
-    this.get('/to_dos', (schema) => {
-        return schema.all('to_dos');
-    });
+
+    this.get('/to_dos', (schema) => ({
+      toDos: schema.all('to_dos').models.map(todoPlain),
+    }));
 
     this.post('/to_dos', (schema, request) => {
       const atributos = JSON.parse(request.requestBody);
       const to_do = schema.create('to_dos', atributos);
-      return to_do;
+      return { toDos: todoPlain(to_do) };
     });
 
     this.put('/to_dos/:id', (schema, request) => {
       const id = request.params.id;
       const novosAtributos = JSON.parse(request.requestBody);
       const to_do = schema.find('to_dos', id);
-      to_do?.update(novosAtributos);
-      return {};
+      if (!to_do) {
+        return new Response(404, {}, { errors: ['not found'] });
+      }
+      to_do.update(novosAtributos);
+      return { toDos: todoPlain(to_do) };
     });
 
     this.delete('/to_dos/:id', (schema, request) => {
       const id = request.params.id;
       const to_do = schema.find('to_dos', id);
       to_do?.destroy();
-      return {};
+      return new Response(204);
     });
   },
 });
