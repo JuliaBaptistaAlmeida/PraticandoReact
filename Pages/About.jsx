7c4f323001860d09dbe8b033df49e21ed0bcf3ec
Julia Baptista <contatojuliabaptista@hotmail.com>
2026-04-22 10:27:29 -0300

    Create About.jsx

---
+++ Pages/About.jsx
@@ -0,0 +1,13 @@
+import { PageLayout } from "../shared/layout/PageLayout";
+import "../App.css";
+
+export function About() {
+  return (
+    <PageLayout title="Sobre">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </PageLayout>
+  );
+}
